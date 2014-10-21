497646f4e2f7427558a63ac53fcd590355cccf24
Shubham Patel <shubhpatel108@gmail.com>
2014-10-21 11:05:40 +0530

    Mix the Shoes app with server and client

--- client.rb
+++ client.rb
@@ -1,21 +1,23 @@
 require 'socket'               # Get sockets from stdlib
-require 'shoes'
-Thread.new do
-  begin
-    hostname = 'localhost'
-    port = 2000
 
-    c1 = TCPSocket.open(hostname, port)
-    $app.simple_para("shubham")      # And print with platform line terminator
+$app = Shoes.app(:width => 256) do
+  Thread.new do
+    begin
+      hostname = 'localhost'
+      port = 2000
 
-    while line = c1.recv(1024)   # Read lines from the socket
-      $app.simple_para(line.chop)      # And print with platform line terminator
-      # cmd = "ls"    #type a command to execute on other client
-      # c1.write cmd
+      c1 = TCPSocket.open(hostname, port)
+      para("shubham")
+
+      while line = c1.recv(1024)   # Read lines from the socket
+        para(line.chop)      # And print with platform line terminator
+        # cmd = "ls"    #type a command to execute on other client
+        # c1.write cmd
+      end
+      c1.close               # Close the socket when done
+
+    rescue => e
+      error(e.to_s)
     end
-    c1.close               # Close the socket when done
-
-  rescue => e
-    $app.error(e.to_s)
   end
 end
--- server.rb
+++ server.rb
@@ -1,24 +1,25 @@
 require 'socket'               # Get sockets from stdlib
-require 'shoes'
 
-Thread.new do
-  begin
-  c2 = TCPServer.open(2000)  # Socket to listen on port 2000
-  loop {                         # Servers run forever
-    c1 = c2.accept
-    $app.simple_para("Client connected!")
-    $app.simple_para(Time.now.to_s)
-    c1.write Time.now.to_s
-    # while line = c1.recv(1024)
-    #   $app.simple_para( "Recevied: " + line.chop)
-    #   # value = %x[#{line}]
-    #   # c1.write value.gsub("\n", "|||")
-    #   # $app.simple_para( "Result was sent!")
-    # end
-    c1.puts "Closing the connection. Bye!"
-    c1.close                 # Disconnect from the client
-  }
-  rescue => e
-    $app.error(e.to_s)
+$app = Shoes.app(:width => 256) do
+  Thread.new do
+    begin
+    c2 = TCPServer.open(2000)  # Socket to listen on port 2000
+    loop {                         # Servers run forever
+      c1 = c2.accept
+      para("Client connected!")
+      para(Time.now.to_s)
+      c1.write Time.now.to_s
+      # while line = c1.recv(1024)
+      #   $app.simple_para( "Recevied: " + line.chop)
+      #   # value = %x[#{line}]
+      #   # c1.write value.gsub("\n", "|||")
+      #   # $app.simple_para( "Result was sent!")
+      # end
+      c1.puts "Closing the connection. Bye!"
+      c1.close                 # Disconnect from the client
+    }
+    rescue => e
+      error(e.to_s)
+    end
   end
 end
