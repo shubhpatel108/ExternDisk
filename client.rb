64b4f631f364af77bf3529964e44e271c387f59f
Shubham Patel <shubhpatel108@gmail.com>
2014-10-21 11:33:28 +0530

    Replace recv and write with gets and puts

--- client.rb
+++ client.rb
@@ -4,16 +4,19 @@ $app = Shoes.app(:width => 256) do
   Thread.new do
     begin
       hostname = 'localhost'
-      port = 2000
+      port = 4000
 
       c1 = TCPSocket.open(hostname, port)
       para("shubham")
 
-      while line = c1.recv(1024)   # Read lines from the socket
-        para(line.chop)      # And print with platform line terminator
-        # cmd = "ls"    #type a command to execute on other client
-        # c1.write cmd
-      end
+      line = c1.gets   # Read lines from the socket
+      para(line.chop)      # And print with platform line terminator
+      cmd = c1.gets   # Read lines from the socket
+      cmd.chop
+      para("recv: #{cmd}")
+      value = %x[#{cmd}]
+      para(value)
+      c1.puts value.gsub("\n", "|||")
       c1.close               # Close the socket when done
 
     rescue => e
--- server.rb
+++ server.rb
@@ -3,19 +3,21 @@ require 'socket'               # Get sockets from stdlib
 $app = Shoes.app(:width => 256) do
   Thread.new do
     begin
-    c2 = TCPServer.open(2000)  # Socket to listen on port 2000
+    c2 = TCPServer.open(4000)  # Socket to listen on port 2000
     loop {                         # Servers run forever
       c1 = c2.accept
       para("Client connected!")
       para(Time.now.to_s)
-      c1.write Time.now.to_s
+      c1.puts Time.now.to_s
+      c1.puts "ls"
+      result = c1.gets
+      para result
       # while line = c1.recv(1024)
       #   $app.simple_para( "Recevied: " + line.chop)
       #   # value = %x[#{line}]
       #   # c1.write value.gsub("\n", "|||")
       #   # $app.simple_para( "Result was sent!")
       # end
-      c1.puts "Closing the connection. Bye!"
       c1.close                 # Disconnect from the client
     }
     rescue => e
